a500897166a95eff77212119ed57266648ee35c8
TaegonAndreaSehoKim <helios473@gmail.com>
2025-06-11 01:25:56 +0900

    Time: 11 ms (54.23%), Space: 12.6 MB (10.4%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -4,13 +4,7 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        if x < 0 or (x % 10 == 0 and x != 0):       # when x is negative number or x's last digit is 0, return false
+        if x < 0:
             return False
-        
-        y = 0       # reverted number variable declaration
-        while x > y:        # until reverted digit exceeds remain digit
-            y = y * 10 + x % 10     # add last digit of x to y
-            x = x // 10     # remove last digit from x
-        
-        return x == y or x == y // 10       # when input's length is odd, we can ignore the middle number by y // 10
+        return x == int(str(x)[::-1])
         
