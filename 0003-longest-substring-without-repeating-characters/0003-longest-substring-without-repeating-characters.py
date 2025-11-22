7e4da0bc96cb360f58f8f9f0a8b9c38942e259ca
TaegonAndreaSehoKim <helios473@gmail.com>
2025-11-22 19:41:42 +0900

    Time: 29 ms (33.57%), Space: 13.3 MB (16.87%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -6,14 +6,19 @@ class Solution(object):
         """
         if len(s) == 0:
             return 0
-        result = 1
+            
+        char_set = set()
         start_idx = 0
+        result = 0
+        
         for i in range(len(s)):
-            if i == 0:
-                continue
-            for j in range(start_idx, i):
-                if s[i] == s[j]:
-                    start_idx = j + 1
+            while s[i] in char_set:
+                char_set.remove(s[start_idx])
+                start_idx += 1
+            
+            char_set.add(s[i])
+            
             result = max(result, i - start_idx + 1)
+            
         return result
                 
