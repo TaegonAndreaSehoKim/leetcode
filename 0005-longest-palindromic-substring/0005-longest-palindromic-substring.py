539328de1500230b709d768e706f190729832c97
TaegonAndreaSehoKim <helios473@gmail.com>
2025-11-22 20:36:07 +0900

    Time: 407 ms (41.88%), Space: 12.3 MB (97.19%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -6,11 +6,17 @@ class Solution(object):
         """
         result = ""
         max_len = 0
-        for l in range(0, len(s)):
-            for r in range(l, len(s)):
-                substr = s[l:r + 1]
-                if substr == substr[::-1]:
-                    if len(substr) > max_len:
-                        result = substr
-                        max_len = len(substr)
+        for i in range(len(s)):
+            for k in range(2): 
+                l = i
+                r = i + k
+                while l >= 0 and r < len(s):
+                    if s[l] == s[r]:
+                        if r - l + 1 > max_len:
+                            result = s[l : r + 1]
+                            max_len = r - l + 1
+                        l = l - 1
+                        r = r + 1
+                    else:
+                        break
         return result
