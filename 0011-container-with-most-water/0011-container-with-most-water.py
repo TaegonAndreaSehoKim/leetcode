c9e466d41f75d9c596b0fdb986eea31c0a587ba2
TaegonAndreaSehoKim <helios473@gmail.com>
2025-11-23 23:05:23 +0900

    Time: 25 ms (98.81%), Space: 20.7 MB (54.88%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -8,11 +8,14 @@ class Solution(object):
         right = len(height) - 1
         result = 0
         while left < right:
-            water_amount = (right - left) * (min(height[left], height[right]))
+            min_height = min(height[left], height[right])
+            water_amount = (right - left) * min_height
             if  water_amount > result:
                 result = water_amount
             if height[left] > height[right]:
-                right = right - 1
+                while left < right and height[right] <= min_height:
+                    right = right - 1
             else:
-                left = left + 1
+                while left < right and height[left] <= min_height:
+                    left = left + 1
         return result
