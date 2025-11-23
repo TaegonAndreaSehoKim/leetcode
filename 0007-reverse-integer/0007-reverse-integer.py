54d2961e907115993550ba8a00b8ba825144b41d
TaegonAndreaSehoKim <helios473@gmail.com>
2025-11-23 21:20:14 +0900

    Time: 20 ms (41.55%), Space: 12.5 MB (6.99%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -8,7 +8,7 @@ class Solution(object):
         abs_number = abs(x)
         result = int(str(abs_number)[::-1])
         if x < 0:
-            result = result * -1
+            result *= -1
         if result > 2**31 - 1 or result < -(2**31):
             return 0
         return result
