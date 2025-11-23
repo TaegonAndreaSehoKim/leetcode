e667f9c65b879f415d448f78797f0da4c7ffe616
TaegonAndreaSehoKim <helios473@gmail.com>
2025-11-23 23:49:59 +0900

    Time: 9 ms (42.74%), Space: 12.4 MB (53.39%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -5,44 +5,15 @@ class Solution(object):
         :rtype: str
         """
         result = ""
-        while num > 0:
-            while num >= 1000:
-                result += "M"
-                num = num - 1000
-            while num >= 900:
-                result += "CM"
-                num = num - 900
-            while num >= 500:
-                result += "D"
-                num = num - 500
-            while num >= 400:
-                result += "CD"
-                num = num - 400
-            while num >= 100:
-                result += "C"
-                num = num - 100
-            while num >= 90:
-                result += "XC"
-                num = num - 90
-            while num >= 50:
-                result += "L"
-                num = num - 50
-            while num >= 40:
-                result += "XL"
-                num = num - 40
-            while num >= 10:
-                result += "X"
-                num = num - 10
-            while num >= 9:
-                result += "IX"
-                num = num - 9
-            while num >= 5:
-                result += "V"
-                num = num - 5
-            while num >= 4:
-                result += "IV"
-                num = num - 4
-            while num >= 1:
-                result += "I"
-                num = num - 1
+        roman_map = [
+            (1000, "M"), (900, "CM"), (500, "D"), (400, "CD"), (100, "C"),
+            (90, "XC"), (50, "L"), (40, "XL"), (10, "X"), (9, "IX"),
+            (5, "V"), (4, "IV"), (1, "I")
+        ]
+        
+        for value, symbol in roman_map:
+            while num >= value:
+                result += symbol
+                num -= value
+
         return result
