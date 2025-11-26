9085e1cea234f82a7332951116af6cf077365e83
TaegonAndreaSehoKim <helios473@gmail.com>
2025-11-27 03:03:36 +0900

    Time: 0 ms (100%), Space: 17.8 MB (56.73%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.py
@@ -0,0 +1,18 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left = len(nums) - 2
+        while left >= 0 and nums[left] >= nums[left + 1]:
+            left -= 1
+        
+        if left >= 0:
+            right = len(nums) - 1
+            while right >= 0 and nums[left] >= nums[right]:
+                right -= 1
+            nums[left], nums[right] = nums[right], nums[left]
+        
+        nums[left + 1:] = reversed(nums[left + 1:])
+
+        return nums
