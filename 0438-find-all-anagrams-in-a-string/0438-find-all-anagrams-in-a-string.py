c2d9a3035f87ccbf0cdd8a8bb038fd23becc20c2
Seho Kim <sehokim@gatech.edu>
2026-07-30 21:09:51 -0700

    Time: 27 ms (88.65%), Space: 19.8 MB (42.2%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        l = 0
+        need = [0] * 26
+        current_window = [0] * 26
+        result = []
+
+        for char in p:
+            need[ord(char) - ord("a")] += 1
+
+        for r, char in enumerate(s):
+            current_window[ord(char) - ord("a")] += 1
+
+            if r - l + 1 > len(p):
+                current_window[ord(s[l]) - ord("a")] -= 1
+                l += 1
+            
+            if need == current_window:
+                result.append(l)
+        
+        return result
