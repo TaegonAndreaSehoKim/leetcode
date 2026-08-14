e0b79f932d75103a713e240e072b7ebf622a4934
Seho Kim <sehokim@gatech.edu>
2026-08-14 16:23:10 -0700

    Time: 263 ms (65.24%), Space: 20.3 MB (15.52%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.py
+++ 0731-my-calendar-ii/0731-my-calendar-ii.py
@@ -1,3 +1,5 @@
+from bisect import bisect_left
+
 class MyCalendarTwo:
 
     def __init__(self):
@@ -5,22 +7,47 @@ class MyCalendarTwo:
         self.double_booked = []
 
     def book(self, startTime: int, endTime: int) -> bool:
-        for double_start, double_end in self.double_booked:
-            if max(double_start, startTime) < min(double_end, endTime):
+        new_booking = [startTime, endTime]
+        index = bisect_left(self.double_booked, new_booking)
+
+        if index > 0:
+            prev_start, prev_end = self.double_booked[index - 1]
+            if prev_end > startTime:
                 return False
 
+        if index < len(self.double_booked):
+            next_start, next_end = self.double_booked[index]
+            if next_start < endTime:
+                return False
+
+        overlaps = []
         for existing_start, existing_end in self.books:
             overlap_start = max(existing_start, startTime)
             overlap_end = min(existing_end, endTime)
-
             if overlap_start < overlap_end:
-                self.double_booked.append(
-                    [overlap_start, overlap_end]
-                )
-        self.books.append([startTime,endTime])
+                overlaps.append([overlap_start, overlap_end])
+
+        for interval in overlaps:
+            self._insert_and_merge(interval)
+
+        self.books.append(new_booking)
         return True
 
+    def _insert_and_merge(self, interval):
+        from bisect import bisect_left
 
-# Your MyCalendarTwo object will be instantiated and called as such:
-# obj = MyCalendarTwo()
-# param_1 = obj.book(startTime,endTime)
+        index = bisect_left(self.double_booked, interval)
+        start, end = interval
+
+        if index > 0 and self.double_booked[index - 1][1] >= start:
+            index -= 1
+            start = min(start, self.double_booked[index][0])
+            end = max(end, self.double_booked[index][1])
+            self.double_booked.pop(index)
+
+        while index < len(self.double_booked) and self.double_booked[index][0] <= end:
+            start = min(start, self.double_booked[index][0])
+            end = max(end, self.double_booked[index][1])
+            self.double_booked.pop(index)
+
+        self.double_booked.insert(index, [start, end])
