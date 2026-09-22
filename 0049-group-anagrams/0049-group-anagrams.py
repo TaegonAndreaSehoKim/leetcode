8e66b22f885443ff06065fe3d5c20901aef4de8d
Seho Kim <sehokim@gatech.edu>
2026-09-22 14:22:06 -0700

    Time: 10 ms (86.83%), Space: 23.9 MB (27.17%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        result = {}
+        for word in strs:
+            chars = [0] * 26
+            for char in word:
+                index = ord(char) - ord('a')
+                chars[index] += 1
+
+            key = tuple(chars)
+
+            if key not in result:
+                result[key] = []
+
+            result[key].append(word)
+        return list(result.values())
