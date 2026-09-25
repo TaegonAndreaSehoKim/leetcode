dd91d06a5680d36488996f6a15c38109f9d33d64
Seho Kim <sehokim@gatech.edu>
2026-09-25 05:27:26 -0700

    Time: 10 ms (26.46%), Space: 18.7 MB (99.95%) - LeetHub

--- 0929-unique-email-addresses/0929-unique-email-addresses.py
+++ 0929-unique-email-addresses/0929-unique-email-addresses.py
@@ -3,15 +3,13 @@ class Solution:
         email_set = set()
         for email in emails:
             local, domain = email.split("@")
-            processed_local = ""           
+            processed_local = ""
             for char in local:
-                if char == "+":
-                    break
-                elif char == ".":
+                if char == ".":
                     continue
+                elif char == "+":
+                    break
                 else:
                     processed_local += char
-
             email_set.add(processed_local + "@" + domain)
-
-        return len(email_set)    
+        return len(email_set)
